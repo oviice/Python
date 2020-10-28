139b51dd2f619f984ef6debba01c56ef294fe33b
Ovi <oviice@gmail.com>
2020-10-28 11:52:43 +0600

    update

--- src/ListDemo.py
+++ src/ListDemo.py
@@ -1,8 +1,8 @@
 list1 = [1, 2, 3, 4, 5]
 list2 = {1, 4, 3, 5}
-for i in list1:
+for i in range(1, 100, 3):
     print(i)
-    if i == 4:
-        continue
+    if i >= 56:
+        break
 print("End of for loop")
 print(type(list1))
