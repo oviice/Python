8e9e7c5576857d401ab0ffacbc277657d2852a11
Ovi <oviice@gmail.com>
2020-11-02 19:04:14 +0600

    module import

--- src/TimeDemo.py
+++ src/TimeDemo.py
@@ -1,6 +1,11 @@
-import time, datetime
+import time, datetime as t, calendar
 
 # returns the formatted time
 
 print(time.asctime(time.localtime(time.time())))
-print(datetime.datetime.now())
+print(t.datetime.now())
+cal = calendar.month(2020, 11)
+print(cal)
+
+cal=calendar.prcal(2020)
+print (cal)
